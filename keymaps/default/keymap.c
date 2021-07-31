6855c7658a7af158372792965ce56574b9a167f0
datapaganism <72796373+datapaganism@users.noreply.github.com>
2021-07-31 09:09:34 +0100

    added more keys on function layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include "afk79.h"
+#include "keymap_uk.h"
 
 enum layers {
   _QWERTY,
@@ -24,15 +25,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 */
 	[_QWERTY] = LAYOUT(
 		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_DEL, 
-		KC_GRV,  KC_1, KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS, KC_EQL, KC_BSPC, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
-		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, MO(1), 
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LALT, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+		KC_GRV,  KC_1, KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS, KC_EQL,     KC_BSPC, 
+		    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,       KC_BSLS, 
+		    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,             KC_ENT, 
+		    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, KC_UP, MO(1), 
+		KC_LCTL, KC_LGUI, KC_LALT,              KC_SPC,              KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
+	/*
+	 * ,--------------------------------------------------------------------------------------------------------.
+	 * | RSET |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Prnt |  INS |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+
+	 * |   `  |   1  |   2  |   3  |   £  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |   =  |  BACKSPACE  |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+
+	 * |     TAB     |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |   \  |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+
+	 * |     CAPS    |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |    ENTER    |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+
+	 * |    SHIFT    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | SHFT | PGUP | FUNC |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+
+	 * | CTRL |  GUI |  ALT |                                                | ALTR | CTRL | HOME | PGDN | END  |
+	 * `--------------------------------------------------------------------------------------------------------'
+	 */
 	[_FUNC] = LAYOUT(
-		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		RESET, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, UK_PND , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, 
