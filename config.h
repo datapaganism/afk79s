6944683fd57689cd49f047ce9eee70a1dd4a7ce8
a2-karnil <alan2.karnil@live.uwe.ac.uk>
2021-10-27 10:08:11 +0100

    updated layout to accomodate split properly

--- config.h
+++ config.h
@@ -12,8 +12,8 @@
 #define DESCRIPTION     Keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 15
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 8
 
 /* key matrix pins */
 //#define UNUSED_PINS
@@ -46,21 +46,19 @@
 
 
 /* SPLIT STUFF */
-#define EE_HANDS
 #define SOFT_SERIAL_PIN D3
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
 
 // from row/col 0 to X
-#define MATRIX_ROW_PINS       { D4, C6, D7, E6, B5, F4 }
+#define MATRIX_ROW_PINS       { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_ROW_PINS_RIGHT { D0, D4, C6 , D7, E6, B4}
 
-#define MATRIX_COL_PINS       { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS       { B6, B2, B3, B1, F7, F6, F5 }
 #define MATRIX_COL_PINS_RIGHT { B5, F4, F5, F6, F7, B1, B3, B2, B6 }
 
 //Experemental
-#define SPLIT_HAND_PIN D0
-#define SPLIT_HAND_PIN_LOW_IS_LEFT
+#define SPLIT_HAND_PIN D2
 
 
 #ifdef RGB_DI_PIN
