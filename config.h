612e79351d63a843f257b69dbeba671a6297b6d9
a2-karnil <alan2.karnil@live.uwe.ac.uk>
2021-10-29 09:59:32 +0100

    release 1.0

--- config.h
+++ config.h
@@ -11,12 +11,6 @@
 #define PRODUCT         afk79s
 #define DESCRIPTION     Split afk79
 
-/* key matrix size */
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 9
-
-/* key matrix pins */
-//#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -46,19 +40,19 @@
 
 
 /* SPLIT STUFF */
-#define SOFT_SERIAL_PIN D3
-#define SPLIT_LAYER_STATE_ENABLE
-#define SPLIT_MODS_ENABLE
+
+/* key matrix size */
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 9
 
 #define MATRIX_ROW_PINS       { D4, C6, D7, E6, B4, B5}
 #define MATRIX_ROW_PINS_RIGHT { D0, D4, C6 ,D7, E6, B4}
 
 #define MATRIX_COL_PINS       { B6, B2, B3, B1, F7, F6, F5, NO_PIN, NO_PIN }
-#define MATRIX_COL_PINS_RIGHT { B5, F4, F5, F6, F7, B1, B3, B2, B6 }
-//#define UNUSED_PINS
-#define SPLIT_HAND_PIN D2
-//#define MASTER_RIGHT
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5, F4, B5 }
 
+#define SOFT_SERIAL_PIN D3
+#define SPLIT_HAND_PIN D2
 #define SPLIT_USB_DETECT
 
 
